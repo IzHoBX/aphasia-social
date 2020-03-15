aeedb63f8073ec89956f13004231f2cfd78cf612
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-15 13:25:44 +0800

    Adds logging verbose

--- hello/views.py
+++ hello/views.py
@@ -9,24 +9,25 @@ import json
 
 AGENT = "/agent?sentence="
 
-count = 0
-
 
 # Create your views here.
 def index(request):
     global count
-    count += 1
-    print("asdvsafvasfgasd" + str(count))
     return render(request, "index.html")
     # return HttpResponse('Hello from Python!')
 
 def f(request):
+    print("XXXX: accepted:" + request.path)
     emoji2Vec = EmojiText.EmojiVec.EmojiVec()
+    print("Emoji2Vec instantiated")
     sentence = getSentence(request.path)
+    print("Received sentence:" + sentence)
 
     ans = []
     for word in KeywordExtract.ExtractKeyword.extractKeyword(sentence):
+        print("extractKeyword:" + word)
         link, score = emoji2Vec.getEmoji(word)
+        print("emoji:" + link)
         ans.append((link, score))
     if len(ans) > RETURN_LIMIT:
         ans.sort(reverse=True, key=takeScore)
@@ -36,6 +37,8 @@ def f(request):
     for i in range(0, len(ans)):
         x[str(i)] = ans[i][0]
     x = json.dumps(x)
+
+    print("sending response...")
 
     res = HttpResponse(x, content_type="application/json")
     res['Access-Control-Allow-Origin'] = '*'
