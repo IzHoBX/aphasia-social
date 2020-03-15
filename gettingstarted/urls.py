3837e6966de600f95ef9a4878f8d6f14793e5ddd
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-15 11:32:27 +0800

    Adds into url acceptance list for new path

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,7 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
+    path("agent", hello.views.f, name="f"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
 from .models import Greeting
+
+AGENT = "/agent"
 
 # Create your views here.
 def index(request):
-    print(request.path)
-    # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
+    # return HttpResponse('Hello from Python!')
+
+def f(request):
+    print(request.path)
+    x = {"a":"B"}
+    x = json.dumps(x)
+    return HttpResponse(bytes(x), content_type="application/json")
 
 
 def db(request):
