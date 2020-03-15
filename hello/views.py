8e1c01c459e6e58a706f3992027b94c29cf93c06
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-15 10:34:31 +0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
+    print(type(request))
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
