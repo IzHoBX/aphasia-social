ff5b26a14960cbf32f55ffd258285495399d3fb0
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-15 12:12:16 +0800

    Brings in driver code from Let's talk

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 import json
 from .models import Greeting
 
-AGENT = "/agent"
+AGENT = "/agent?sentence="
+
 count = 0
 
 
@@ -16,10 +17,34 @@ def index(request):
     # return HttpResponse('Hello from Python!')
 
 def f(request):
-    print(request.path)
-    x = {"a":"B"}
+    emoji2Vec = EmojiText.EmojiVec.EmojiVec()
+    sentence = getSentence(request.path)
+
+    ans = []
+    for word in KeywordExtract.ExtractKeyword.extractKeyword(sentence):
+        link, score = emoji2Vec.getEmoji(word)
+        ans.append((link, score))
+    if len(ans) > RETURN_LIMIT:
+        ans.sort(reverse=True, key=takeScore)
+        ans = ans[:RETURN_LIMIT]
+
+    x = {}
+    for i in range(0, len(ans)):
+        x[str(i)] = ans[i][0]
     x = json.dumps(x)
-    return HttpResponse(x, content_type="application/json")
+
+    res = HttpResponse(x, content_type="application/json")
+    res['Access-Control-Allow-Origin'] = '*'
+
+    return
+
+
+def getSentence(inx):
+    inx = inx[len(PREFIX):]
+    for i in range(0, len(inx)):
+        if inx[i] == "+":
+            inx = inx[:i]+ " "+inx[i+1:]
+    return inx
 
 
 def db(request):
