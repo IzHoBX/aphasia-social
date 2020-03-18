4b5f0a36b232d9ddccc7106b93c68c0b150b44b1
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-03-18 19:26:42 +0800

    Mitigates same vector issue for emojis by replotting space with large model

--- EmojiText/resetspace.py
+++ EmojiText/resetspace.py
@@ -8,7 +8,7 @@ SPACE_PATH = "emojispace"
 NAME2LINK_PATH = "emojilib"
 
 nameToLink = pickle.load(open(NAME2LINK_PATH, "rb"))
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_lg")
 allVectors = []
 indexToName = []
 
@@ -17,8 +17,14 @@ for name, link in nameToLink.items():
     if token.vector_norm == 0:
         print(name)
     else:
-        allVectors.append(token.vector)
-    indexToName.append(name)
+        foundSharedVec = False
+        for i in range(0, len(allVectors)):
+            if numpy.array_equal(allVectors[i], token.vector):
+                print(indexToName[i] + " " + name)
+                foundSharedVec = True
+        if not foundSharedVec:
+            allVectors.append(token.vector)
+            indexToName.append(name)
 allVectors = numpy.array(allVectors)
 nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(allVectors)
 pickle.dump([nbrs, indexToName], open(SPACE_PATH, "wb"))
