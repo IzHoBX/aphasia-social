3ab0c372e05b85d88c8a952b83db75cc759334ed
Ho Bing Xuan <hobingxuan97@gmail.com>
2020-06-09 20:46:51 +0800

    fixes bug on corrupted emoji link for emojis with multi-word description

--- EmojiText/UpdateAnnotate.py
+++ EmojiText/UpdateAnnotate.py
@@ -6,7 +6,11 @@ nameToLink = {}
 
 for line in f:
     data = line.split(",")
-    nameToLink[data[0]] = data[1] + "," + data[2]
+    indexForLink = 1
+    while not "data:image/png;base64" in data[indexForLink]:
+        indexForLink += 1
+    name = "".join(map(lambda x: x + " ", data[:indexForLink]))[:-1]
+    nameToLink[name] = data[indexForLink] + "," + data[indexForLink+1]
 
 pickle.dump(nameToLink, open("emojilib", "wb"))
 f.close()
